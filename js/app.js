51009b26ab5ab89892a937636c91150af4ac4855
czumczum <e.mindjunk@gmail.com>
2016-08-09 22:18:33 +0200

    jQuery: animated skills

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
     //Variables
+    var check;
+    var navOff;
+    var checkMenu;
+    var scrollMe;
+    var refElement;
+    var menu, navMenu;
     var nav = $('nav');
+    var scrollIcon = $('.scroll');
 
     //Sticky menu on top when scrolling
-    var navOff = nav.offset().top;
-    var check = function() {
+    navOff = nav.offset().top;
+    check = function () {
         var scroll = $(document).scrollTop();
         if (navOff <= scroll) {
             nav.addClass('sticky');
@@ -15,9 +22,9 @@ $(document).ready(function() {
     };
 
     //Navigation menu (two places included)
-    var menu = $('header').find('.img'),
-        navMenu = nav.find('li');
-    var scrollMe = function(e) {
+    menu = $('header').find('.img');
+    navMenu = nav.find('li');
+    scrollMe = function () {
         var name = "." + $(this).attr('data-id'); //"." for class
         $('html, body').animate({
             scrollTop: $(name).offset().top
@@ -25,17 +32,17 @@ $(document).ready(function() {
     };
 
     //Activate the bottom border on active section in sticky menu
-    var checkMenu = function(e) {
+    checkMenu = function () {
         var scroll = $(document).scrollTop();
         nav.find('li').each(function () {
             var currLink = $(this);
-            var refElement = $("." + currLink.attr("data-id"));
+            refElement = $("." + currLink.attr("data-id"));
             console.log(refElement);
-            if (refElement.position().top <= scroll && refElement.position().top + refElement.height() > scroll) {
+            if (refElement.position().top - 100 <= scroll && refElement.position().top + refElement.height() + 800 > scroll) {
                 nav.find('li').removeClass("active");
                 currLink.addClass("active");
             }
-            else{
+            else {
                 currLink.removeClass("active");
             }
         });
@@ -49,5 +56,25 @@ $(document).ready(function() {
     $(window).on('resize', check);
     menu.on("click", scrollMe);
     navMenu.on("click", scrollMe);
+    scrollIcon.on("click", function () {
+        $('html, body').animate({
+            scrollTop: $('nav').offset().top
+        }, 1000);
+    })
 
+    //Animated skillset
+    var skills = $('.skillFilling');
+    var elementID;
+    var value;
+    var animateSkills = function () {
+        skills.each(function () {
+            console.log($(this));
+            elementID = $(this).attr('data-skill');
+            value = $(document.getElementById(elementID)).text();
+            $(this).animate({
+                width: value
+            }, 5000)
+        })
+    }
+    animateSkills();
 });
