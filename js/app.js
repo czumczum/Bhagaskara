8c16ffbea3f3cd189f9930e66b7e1eec01e94c7f
czumczum <e.mindjunk@gmail.com>
2016-08-10 15:00:31 +0200

    Carousel

--- js/app.js
+++ js/app.js
@@ -37,7 +37,6 @@ $(document).ready(function() {
         nav.find('li').each(function () {
             var currLink = $(this);
             refElement = $("." + currLink.attr("data-id"));
-            console.log(refElement);
             if (refElement.position().top - 100 <= scroll && refElement.position().top + refElement.height() + 800 > scroll) {
                 nav.find('li').removeClass("active");
                 currLink.addClass("active");
@@ -60,21 +59,46 @@ $(document).ready(function() {
         $('html, body').animate({
             scrollTop: $('nav').offset().top
         }, 1000);
-    })
+    });
 
-    //Animated skillset
+    //Animated skills
     var skills = $('.skillFilling');
     var elementID;
     var value;
     var animateSkills = function () {
         skills.each(function () {
-            console.log($(this));
             elementID = $(this).attr('data-skill');
             value = $(document.getElementById(elementID)).text();
             $(this).animate({
                 width: value
             }, 5000)
         })
-    }
+    };
     animateSkills();
+
+    //Carousel
+    var carousel = $('.carousel-content');
+    var dotts = $('.testimony').find('ul');
+    var dott;
+    carousel.each(function (val) {
+        $(this).attr('id', val);
+        dott = $('<li>');
+        dott.prependTo(dotts);
+    });
+    dott.first().addClass('active');
+    var merryGoRound = function () {
+        var current = $('.visible');
+        var currentDott = $('.active');
+        current.removeClass("visible");
+        currentDott.removeClass('active');
+        if (current.attr('id') != 2) {
+            current.next().addClass("visible");
+            currentDott.next().addClass('active');
+        } else {
+            $('#0').addClass('visible');
+            dott.first().addClass('active');
+
+        }
+    };
+    carousel.on("click", merryGoRound);
 });
